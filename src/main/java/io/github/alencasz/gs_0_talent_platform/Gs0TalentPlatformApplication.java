8dfec714cf604b25755c6abaf35390995640feba
alencasz <lucaspereira995@outlook.com>
2025-11-13 18:33:32 -0300

    Adicionado esqueleto do projeto Spring Boot para a Global Solution

---
+++ src/main/java/io/github/alencasz/gs_0_talent_platform/Gs0TalentPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.alencasz.gs_0_talent_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs0TalentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs0TalentPlatformApplication.class, args);
+	}
+
+}
